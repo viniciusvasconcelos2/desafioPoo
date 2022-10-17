96a3a08a8adf09a0bdc9ac4b6d0b2358002b939d
viniciusvasconcelos2 <viniciusvasconcelos2@gmail.com>
2022-10-17 01:35:02 -0300

    mentoria e cursos

---
+++ src/edu/vhinny/desafioPoo/dominio/Conteudo.java
@@ -0,0 +1,24 @@
+package edu.vhinny.desafioPoo.dominio;
+
+public abstract class Conteudo {
+     protected static final  Double XP_PADRAO = 10d;
+    private String titulo;
+    private String descricao;
+    public  abstract double calcularXP();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
